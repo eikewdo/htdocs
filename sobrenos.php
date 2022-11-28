26c54c3e1739712821f99735b087fb489e757181
eikewdo <70212275+eikewdo@users.noreply.github.com>
2022-11-28 03:23:22 -0300

    Add files via upload

---
+++ sobrenos.php
@@ -0,0 +1,169 @@
+<?php
+$title = 'Sobre nós';
+include 'head.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <meta charset="utf-8">
+
+</head>
+
+<body class="fundo">
+
+    <h1 class="titulosobre">Sobre Nós</h1>
+    <br>
+    <div>
+        <h5 class="textosobre">O Safe Map é um projeto desenvolvido por alunos do curso de Análise e Desenvolvimento de
+            Sistemas da Universidade Federal do Rio Grande do Norte (UFRN) como parte da disciplina de Projeto
+            Integrador II. O projeto tem como objetivo principal a criação de um aplicativo que auxilie na prevenção de
+            acidentes de trânsito, principalmente em relação a acidentes com motociclistas. O aplicativo tem como
+            público alvo os motociclistas, que poderão se cadastrar no aplicativo e informar a localização de acidentes
+            ocorridos na cidade de Natal, assim como a gravidade do acidente. O aplicativo também terá um mapa que
+            mostrará a localização dos acidentes ocorridos na cidade, assim como a gravidade do acidente. O aplicativo
+            também terá um mapa que mostrará a localização dos acidentes ocorridos na cidade, assim como a gravidade do
+            acidente. O aplicativo também terá um mapa que mostrará a localização dos acidentes ocorridos na cidade,
+            assim como a gravidade do acidente. O aplicativo também terá um mapa que mostrará a localização dos
+            acidentes ocorridos na cidade, assim como a gravidade do acidente. O aplicativo também terá um mapa que
+            mostrará a localização dos acidentes ocorridos na cidade, assim como a gravidade do acidente. O aplicativo
+            também terá um mapa que mostrará a localização dos acidentes ocorridos na cidade, assim como a gravidade do
+            acidente. O aplicativo também terá um mapa que mostrará a localização dos acidentes ocorridos na cidade,
+            assim como a gravidade do acidente. O aplicativo também terá um mapa que mostrará a localização dos
+            acidentes ocorridos na cidade, assim como a gravidade do acidente. O aplicativo também terá um mapa que
+            mostrará a localização dos acidentes ocorridos na cidade, assim como a gravidade do acidente. O aplicativo
+            também terá um mapa que mostrará a localização dos acidentes ocorridos na cidade, assim como a gravidade do
+            acidente. O aplicativo também terá um mapa que mostrará a local</h5>
+    </div>
+<br>
+    <div class="row">
+        <div id="centro" class="col-md-12 col-sm-6">
+            <img id="programadores" src="assets\imagens\usergraysemfundo.png" width="150px" height="150pt">
+            <br>
+            <p>Eike Wéslley de Oliveira 
+                <br>
+                Programador
+                <br>
+                <a href="https://www.linkedin.com/in/eike-weslley/"> <img src="assets\imagens\linkedin.png" width="20px"
+                        height="20px"> </a>
+                <a href="https://github.com/eikewdo"> <img src="assets\imagens\github2.png" width="20px" height="20px">
+                </a>
+            </p>
+        </div>
+        <br>
+        <div id="centro" class="col-md-12 col-sm-6">
+            <img id="programadores" src="assets\imagens\usergraysemfundo.png" width="150px" height="150pt">
+            <br>
+            <p>Isabella Miranda da Silva
+                <br>
+                Designer
+                <br>
+                <a href="https://br.linkedin.com/in/eike-weslley-198114238"> <img src="assets\imagens\linkedin.png"
+                        width="20px" height="20px"> </a>
+                <a href="https://github.com/eikewdo"> <img src="assets\imagens\github2.png" width="20px" height="20px">
+                </a>
+            </p>
+        </div>
+        <br>
+        <div id="centro" class="col-md-12 col-sm-6">
+            <img id="programadores" src="assets\imagens\usergraysemfundo.png" width="150px" height="150pt">
+            <br>
+            <p>Júlia Alves Theodoro
+                <br>
+                Designer
+                <br>
+                <a href="https://www.linkedin.com/in/eike-weslley/"> <img src="assets\imagens\linkedin.png" width="20px"
+                        height="20px"> </a>
+                <a href="https://github.com/eikewdo"> <img src="assets\imagens\github2.png" width="20px" height="20px">
+                </a>
+            </p>
+        </div>
+        <br>
+        <div id="centro" class="col-md-12 col-sm-6">
+            <img id="programadores" src="assets\imagens\usergraysemfundo.png" width="150px" height="150pt">
+            <br>
+            <p>Nicolas Brüski
+                <br>
+                Programador
+                <br>
+                <a href="https://www.linkedin.com/in/eike-weslley/"> <img src="assets\imagens\linkedin.png" width="20px"
+                        height="20px"> </a>
+                <a href="https://github.com/eikewdo"> <img src="assets\imagens\github2.png" width="20px" height="20px">
+                </a>
+            </p>
+        </div>
+        <br>
+        <div id="centro" class="col-md-12 col-sm-6">
+            <img id="programadores" src="assets\imagens\usergraysemfundo.png" width="150px" height="150pt">
+            <br>
+            <p>Ruhan Kaio Schitz
+                <br>
+                Programador
+                <br>
+                <a href="https://www.linkedin.com/in/eike-weslley/"> <img src="assets\imagens\linkedin.png" width="20px"
+                        height="20px"> </a>
+                <a href="https://github.com/eikewdo"> <img src="assets\imagens\github2.png" width="20px" height="20px">
+                </a>
+            </p>
+        </div>
+        <br>
+        <div id="centro" class="col-md-12 col-sm-6">
+            <img id="programadores" src="assets\imagens\usergraysemfundo.png" width="150px" height="150pt">
+            <br>
+            <p>Richard Pereira Fernandes
+                <br>
+                programador
+                <br>
+                <a href="https://www.linkedin.com/in/eike-weslley/"> <img src="assets\imagens\linkedin.png" width="20px"
+                        height="20px"> </a>
+                <a href="https://github.com/eikewdo"> <img src="assets\imagens\github2.png" width="20px" height="20px">
+                </a>
+            </p>
+        </div>
+        <br>
+    </div>
+    </div>
+    </div>
+</body>
+
+</html>
+
+<style>
+    .fundo {
+        background-color: #101020;
+
+    }
+
+    .titulosobre {
+        color: white;
+        font-size: 50px;
+        text-align: center;
+        
+    }
+
+    .textosobre {
+        color: white;
+        margin-left: 40px;
+        margin-right: 40px;
+        font-size: 15px;
+        text-align: center;
+        
+       
+    }
+
+    #centro {
+        margin-left: 0px;
+        color: white;
+        text-align: center;
+    }
+
+    #programadores {
+        padding: 10px;
+        background-size: 100%;
+    }
+</style>
+<?php
+include "footer.php";
+?>
